4b66ea0a3becce34200fc18cf7bca3af203f0f85
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-02-04 15:13:14 +0100

    refactor loadCollects method to simplify data retrieval from localStorage and remove unnecessary comments

--- src/app/pages/individual/collects/collects.component.ts
+++ src/app/pages/individual/collects/collects.component.ts
@@ -15,14 +15,11 @@ export class CollectsComponent implements OnInit {
   }
 
   loadCollects() {
-    // Récupérer les données depuis localStorage
     const collectsData = localStorage.getItem('collecteData');
 
     if (collectsData) {
-      // Convertir en tableau d'objets
       this.collects = JSON.parse(collectsData);
 
-      // Trier les collectes par date en ordre décroissant
       this.collects.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
     }
   }
