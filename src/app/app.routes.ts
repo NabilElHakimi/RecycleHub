cbfd000943420ca8a2684112d0dac7c1057cd9a5
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-02-05 12:32:46 +0100

    add profile component with layout and styling; implement routing and create initial test setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { DemandeCollecteComponent } from './pages/individual/demande-collecte/demande-collecte.component';
 import { CollectsComponent } from './pages/individual/collects/collects.component';
 import {HomeComponent} from './pages/home/home.component';
+import {ProfileComponent} from './pages/profile/profile.component';
 
 export const routes: Routes = [
 
@@ -13,6 +14,7 @@ export const routes: Routes = [
     children : [
        {path: '', redirectTo: 'home', pathMatch: 'full'},
        {path : 'home' , component : HomeComponent },
+       {path : 'profile' , component : ProfileComponent },
        {path : 'collects' , component : CollectsComponent },
        {path : 'demand-collect' , component : DemandeCollecteComponent },
     ] }
